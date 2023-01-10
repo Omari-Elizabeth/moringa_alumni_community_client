750f779ce442e27af7e816d1f463cf8359be027e
catherineotondi <catherineotondi@gmail.com>
2023-01-10 14:40:40 +0300

    add an image and a paragraph

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 // First Screen to appear to user.
 import Footer from "./Footer";
 import { Navigation } from "./MainDisplay";
+import Logo from "../img/homelogo.png"
 
 
 function Home(){
@@ -9,7 +10,12 @@ function Home(){
             <Navigation />
             <div>
                 <div>
-                    Mission Statement with background picture here
+                <img src={Logo} alt="logo"  width={100} height={100} />
+                <p>
+                Many graduates struggle with getting opportunities for work and mentorship while
+                others have opportunities they want to share with their fellow former students and
+                often have no platform to do that.
+            </p>
                 </div>
             </div>
             <Footer />
