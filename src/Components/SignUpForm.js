4d85890887759c897fa5771d3b546b83e73960f7
catherineotondi <catherineotondi@gmail.com>
2023-01-12 03:35:15 +0300

    style login file using tailwind css

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -19,10 +19,10 @@ function SignUp(){
             <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-3 border-solid border-white border-2 w-2/4 m-auto bg-cloud-burst-600 mt-2">
 
             <div className="m-auto ">
-                <img src={Logo} alt="logo" className="border rounded-xl" width={100} height={100} />
+                <img src={Logo} alt="logo" className="border rounded-xl mt-3" width={100} height={100} />
                 </div>
                 
-                <h1 className="text-xl  text-international-orange-600">SIGN UP</h1>
+                <h1 className="text-xl  text-international-orange-600 font-bold" >SIGN UP</h1>
                 <label className="text-white text-lg">
                     Username 
                 <input type="text" 
@@ -49,7 +49,7 @@ function SignUp(){
                 
                 <div className="flex justify-center gap-5">
                 <h3> Have an Account? <Link to='/login' className="text-international-orange-600"> Log In </Link> </h3>
-                <h3> <Link to='/home'>Return To <span className="text-international-orange-600"> Home</span></Link> </h3>
+                <h3> <Link to='/home'>Return To <span className="text-international-orange-600 font-bold"> Home</span></Link> </h3>
             </div>
 
             </form>
