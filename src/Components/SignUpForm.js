2bd75e08bd68c30f0c7c65e10ee47e1e6c587a12
catherineotondi <catherineotondi@gmail.com>
2023-01-16 15:41:17 +0300

    edit the adminstrator link and centralized the form

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -48,7 +48,7 @@ function SignUp( { user , updateUser }){
 
     return(
         <section className="min-h-full">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-3 border-solid border-white border-2 w-2/4 m-auto bg-cloud-burst-600 mt-2">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-3  rounded-3xl border-solid border-white border-2 w-2/4 m-auto bg-cloud-burst-600 mt-20">
 
             <div className="m-auto ">
                 <img src={Logo} alt="logo" className="border rounded-xl mt-3" width={100} height={100} />
