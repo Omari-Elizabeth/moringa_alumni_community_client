e7fe010101cbe77fb5e094e5287367135ab91a1a
MurituMaina <vincentmaina0@gmail.com>
2023-01-18 00:05:02 +0300

    add username and posts divs

--- src/Components/AdminPostsDashboard.js
+++ src/Components/AdminPostsDashboard.js
@@ -10,11 +10,19 @@ function AdminAnnouncementsDashboard() {
         setPosts(jsonfile);
       });
   }, [apiUrl]);
+  // function handleDelete(id) {
+  //   // e.preventDefault();
+  //   fetch(`/posts/${id}`, {
+  //     method: "DELETE",
+  //   })
+  //     .then((resp) => resp.json())
+  //     .then((res) => console.log(res));
+  // }
 
   return (
     <>
       <div className="flex">
-        <aside className="w-44 fixed left-0 top-0 h-screen bg-slate-700 p-10">
+        <aside className="w-44 fixed left-0 top-0 h-screen bg-[#242747] p-10">
           <h2 className="text-sky-50">Admin Dashboard</h2>
           <div>
             <br />
@@ -36,33 +44,30 @@ function AdminAnnouncementsDashboard() {
           </div>
         </aside>
         <div>
-          <div className="w-1/2 absolute top-0 left-45 right-20 bg-[#242740] ">
+          <div className="w-1/2 absolute top-0 left-45 right-20  ">
             <h1 className="font-bold text-2xl">Posts</h1> <br />
             <br />
-            <table className="border-collapse:collapse;">
-              <thead>
-                <tr className="bg-blue-50">
-                  <th className="border p-2 ">Post ID</th>
-                  <th className="border p-2 ">Username</th>
-                  <th className="border p-2">Content</th>
-                  <th className="border p-2 ">Action</th>
-                </tr>
-              </thead>
-              {posts.map((post, i) => {
-                return (
-                  <tbody key={i} value={post}>
-                    <tr key={i} value={post}>
-                      <td className="border ">{post.id}</td>
-                      <td className="border  p-2 ">{post.title}</td>
-                      <td className="border">{post.content}</td>
-                      <td className="border  p-2">
-                        <button className="text-blue-600">Delete</button>
-                      </td>
-                    </tr>
-                  </tbody>
-                );
-              })}
-            </table>
+            {posts.map((post, i) => {
+              return (
+                <div className="bg-[#242747] p-5 space-y-4 rounded-lg" key={i}>
+                  {console.log(post.id)}
+                  <h2 className="font-bold">{post.user.username}</h2>
+                  <button
+                    // onClick={handleDelete(post.id)}
+                    className="bg-red-600 p-1 rounded-lg space-x-2"
+                  >
+                    {/* Delete */}X
+                  </button>
+                  <h2 className=" text-blue-500 text-left">{post.title}</h2>{" "}
+                  <p>{post.content}</p>
+                  <p>{post.likes} Likes</p>
+                  <div className="bg-white">
+                    {" "}
+                    <br />{" "}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
